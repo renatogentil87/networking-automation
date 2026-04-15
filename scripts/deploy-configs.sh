725099b7cca1d1d595abee70f18c9bfab8fbcec4
Renato Gentil <112764390+rrdog-aws@users.noreply.github.com>
2026-04-15 12:15:47 +0100

    pushing topology file to the instance

--- scripts/deploy-configs.sh
+++ scripts/deploy-configs.sh
@@ -4,6 +4,6 @@ set -e
 cd /home/ubuntu/network-lab
 
 echo "Deploying BGP configurations..."
-ansible-playbook playbooks/deploy-bgp.yml -i inventory/lab.ini
+ansible-playbook playbooks/deploy-bgp.yml -i inventory/lab.ini ---vvv
 
 echo "✓ Deployment completed"
